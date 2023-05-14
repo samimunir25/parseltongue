b57e2f4b62bd95f32b40b606d6845eebd14b298b
padfoot.black <padfoot@cybertron.io>
2023-05-14 19:35:45 -0400

    docs: update comments in the code

--- 1.Loops_and_Iterations/find_smallest_and_largest_number.py
+++ 1.Loops_and_Iterations/find_smallest_and_largest_number.py
@@ -2,6 +2,8 @@
 print("Welcome to the program: 'Identify the smallest and largset number in the given range'\n")
 my_list = (input("Enter numbers seperated by blank spaces, and then hit enter: "))
 
+# Process the input, split the list of strings, and then convert the elements into integer
+
 my_list = my_list.split()
 my_list = list(map(int, my_list))
 print(my_list)
@@ -9,6 +11,8 @@ print("\n")
 
 smallest_number = None
 largest_number = None
+
+# Definite loop nested with conditional statements
 
 for item in my_list:
     if smallest_number is None:
@@ -22,6 +26,7 @@ for item in my_list:
     elif item > largest_number:
         largest_number = item
 
+# Print results
 
 print("Smallest number is: ", smallest_number)
 print("Largest number is: ", largest_number)  
