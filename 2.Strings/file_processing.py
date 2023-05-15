7a8984453285332cb7e040d6ed1a1b96f063e779
padfoot.black <padfoot@cybertron.io>
2023-05-14 23:45:04 -0400

    fix: counting lines and characters in text file

--- 2.Strings/file_processing.py
+++ 2.Strings/file_processing.py
@@ -1,4 +1,7 @@
-FILE_NAME = input("Enter name of file for processing: ")
+'''File processing code
+'''
+# # Find number of lines in file
+FILE_NAME = input("Enter name of file for counting number of lines: ")
 XFILE = open(FILE_NAME)
 
 LINE_COUNT = 0
@@ -7,4 +10,13 @@ for lines in XFILE:
     LINE_COUNT += 1
 
 print("\n")
-print(f'total number of lines in', FILE_NAME , 'is :', LINE_COUNT)
+print(f'total number of lines in {FILE_NAME} is {LINE_COUNT}')
+
+# Reading the *whole* file
+print("\n")
+FILE_NAME = input("Enter name of file for counting total number of characters: ")
+OPEN_FILE = open(FILE_NAME)
+READ_FILE = OPEN_FILE.read()
+TOTAL_CHAR = len(READ_FILE)
+print("\n")
+print(f'Total number of characters in {FILE_NAME} is {TOTAL_CHAR}')
