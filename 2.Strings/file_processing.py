5a7e400b57a59885c1f8b499c3cbd834cbcac928
padfoot.black <padfoot@cybertron.io>
2023-05-15 00:03:47 -0400

    fix: add try/except block

--- 2.Strings/file_processing.py
+++ 2.Strings/file_processing.py
@@ -2,7 +2,11 @@
 '''
 # # Find number of lines in file
 FILE_NAME = input("Enter name of file for counting number of lines: ")
-XFILE = open(FILE_NAME)
+try:
+    XFILE = open(FILE_NAME)
+except:
+    print(f'file {FILE_NAME} cannot be found')
+    quit()
 
 LINE_COUNT = 0
 
@@ -15,8 +19,28 @@ print(f'total number of lines in {FILE_NAME} is {LINE_COUNT}')
 # Reading the *whole* file
 print("\n")
 FILE_NAME = input("Enter name of file for counting total number of characters: ")
-OPEN_FILE = open(FILE_NAME)
+try:
+    OPEN_FILE = open(FILE_NAME)
+except:
+    print(f'file {FILE_NAME} cannot be found')
+    quit()
+
 READ_FILE = OPEN_FILE.read()
 TOTAL_CHAR = len(READ_FILE)
 print("\n")
 print(f'Total number of characters in {FILE_NAME} is {TOTAL_CHAR}')
+
+# Searching through file
+print("\n")
+FILE_NAME = input("Enter name of file for searching special lines: ")
+try:
+    OPEN_FILE = open(FILE_NAME)
+except:
+    print(f'file {FILE_NAME} cannot be found')
+    quit()
+
+for LINE in OPEN_FILE:
+    LINE = LINE.rstrip()
+    if LINE.startswith('From'):
+        print(LINE)
+
